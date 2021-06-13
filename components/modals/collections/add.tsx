852791f948affed30cf4a62b01fe84657ec5b50f
Connor Davis <19422@kloofhigh.co.za>
2021-06-13 09:26:09 +0200

    minor ui stuff for collections started

--- components/modals/collections/add.tsx
+++ components/modals/collections/add.tsx
@@ -1,23 +1,34 @@
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog, Menu, Transition } from '@headlessui/react';
+import React, { useEffect, useState } from 'react';
 
-import React from 'react';
+import SearchIcon from '../../icons/search';
 
 type AddCollectionModalProps = {
+  collections?: any;
   show?: boolean;
   onAdd?: Function;
   onCancel?: Function;
 };
 
 let AddCollectionModal = ({
+  collections,
   show,
   onAdd,
   onCancel,
 }: AddCollectionModalProps) => {
+  let [keyWord, setKeyWord] = useState('First Name');
+  let [keyWords, setKeyWords] = useState([
+    'First Name',
+    'Last Name',
+    'Phone Number',
+    'ID Number',
+  ]);
+
   return (
     <Transition show={show} as={React.Fragment}>
       <Dialog
         as="div"
-        className="fixed inset-0 z-20 overflow-y-auto"
+        className="fixed inset-0 z-20 overflow-y-auto w-auto"
         static
         open={show}
         onClose={() => {}}
@@ -52,23 +63,63 @@ let AddCollectionModal = ({
           >
             <div className="inline-block w-auto p-3 my-8 overflow-hidden text-left align-middle transition-all transform bg-gray-100 dark:bg-black shadow-xl rounded-2xl border-l border-t border-r border-b border-gray-300 dark:border-gray-800">
               <div className="bg-gray-100 dark:bg-black">
-                <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900 dark:text-white text-center mb-5"
-                >
-                  Add Collection
-                </Dialog.Title>
                 <div className="mt-2">
                   <p className="text-sm text-gray-500 dark:text-gray-400"></p>
+
+                  <div className="flex w-full items-center px-2 py-1">
+                    <div className="px-2 py-2 h-full bg-gray-200 text-green-600">
+                      <SearchIcon />
+                    </div>
+                    <input
+                      className="px-2 py-1 bg-gray-200 outline-none"
+                      placeholder="Search Collector"
+                    />
+                    <div className="px-2 py-1 w-full h-full bg-gray-200 text-green-600">
+                      <Menu
+                        as="div"
+                        className="relative inline-block text-left"
+                      >
+                        <div>
+                          <Menu.Button className="focus:outline-none border-0">
+                            {keyWord}
+                          </Menu.Button>
+                        </div>
+                        <Transition
+                          as={React.Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 w-40 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none h-16 overflow-y-auto">
+                            <div className="px-1 py-1">
+                              {keyWords.map((key) => (
+                                <Menu.Item key={key}>
+                                  <div
+                                    className="flex items-center justify-start px-2 py-1 cursor-pointer hover:bg-green-300 hover:text-green-600 transition duration-500 ease-in-out focus:outline-none rounded-md"
+                                    onClick={() => setKeyWord(key)}
+                                  >
+                                    {key}
+                                  </div>
+                                </Menu.Item>
+                              ))}
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+                  </div>
                 </div>
 
                 <div className="flex justify-center items-center mt-4 w-full space-x-2">
                   <button
                     type="button"
                     className="w-full inline-flex justify-center px-4 py-2 text-sm font-medium text-green-900 bg-green-100 border border-transparent rounded-md hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-green-500"
-                    onClick={() => onAdd()}
+                    onClick={() => {}}
                   >
-                    Add
+                    Continue
                   </button>
 
                   <button
